e8cc92358c7877f56451bfc32b0f533dad7bf116
Abdul <120697467+Abdulftahm@users.noreply.github.com>
2025-08-30 16:27:53 -0400

    Update settings.py

--- Book/BookProject/BookProject/settings.py
+++ Book/BookProject/BookProject/settings.py
@@ -15,7 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'library',  # تأكد أنها مذكورة مرة واحدة فقط
+    'BookProject.library',  # تأكد أنها مذكورة مرة واحدة فقط
 ]
 
 MIDDLEWARE = [
