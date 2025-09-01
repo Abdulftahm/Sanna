0901175b65ba0d2c5312abaed5048315225f3a83
Abdul <120697467+Abdulftahm@users.noreply.github.com>
2025-08-31 21:04:46 -0400

    Update settings.py

--- Book/BookProject/settings.py
+++ Book/BookProject/settings.py
@@ -1,11 +1,11 @@
 import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(_file_).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-your-secret-key-here'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]  # السماح لأي دومين (مهم لـ Railway)
 LOGIN_URL = '/login/'
 
 INSTALLED_APPS = [
@@ -74,11 +74,14 @@ TIME_ZONE = 'Asia/Riyadh'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# ---------------------------
+# Static & Media
+# ---------------------------
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # حل مشكلة collectstatic
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # لو عندك static مخصصة
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
