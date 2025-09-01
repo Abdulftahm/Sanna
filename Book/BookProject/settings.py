8e25a9884fef4a2124935a9ce80750d3428cd1f1
Abdul <120697467+Abdulftahm@users.noreply.github.com>
2025-08-31 20:46:34 -0400

    Update settings.py

--- Book/BookProject/settings.py
+++ Book/BookProject/settings.py
@@ -15,7 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'library',  # تأكد أنها مذكورة مرة واحدة فقط
+    'BookProject.library',  # تأكد أنها مذكورة مرة واحدة فقط
 ]
 
 MIDDLEWARE = [
@@ -79,5 +79,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
