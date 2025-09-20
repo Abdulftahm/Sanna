313083fd458b52cb1b5a8f4509c9f37fbb2f034c
Abdul <120697467+Abdulftahm@users.noreply.github.com>
2025-09-19 20:36:58 -0400

    Delete Book/library directory

--- Book/library/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Category, Book, UserLibrary
-from .models import SEOSettings
-
-
-admin.site.register(SEOSettings)
-admin.site.register(Category)
-admin.site.register(Book)
-admin.site.register(UserLibrary)
