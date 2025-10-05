0b67049a6b18b66e8d558fede1aecee2887a49de
mitchell <mitchell>
2025-10-06 06:27:56 +1300

    updates

--- Book/BookProject/production.py
+++ Book/BookProject/production.py
@@ -6,6 +6,14 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = False
 ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
 CSRF_TRUSTED_ORIGINS=[f"https://{x}" for x in ALLOWED_HOSTS]
+DOCKER_VOL_PATH='/mnt/storage'
 
-STATIC_ROOT="/mnt/docker-vol/static"
+STATIC_ROOT=f"{DOCKER_VOL_PATH}/static"
 STATICFILES_STORAGE="whitenoise.storage.CompressedStaticFilesStorage"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': f"{DOCKER_VOL_PATH}/db/db.sqlite3",
+    }
+}
